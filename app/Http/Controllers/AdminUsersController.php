270016391c1cb796a97260db290a8106c8faad4b
songdew <raghu@songdewnetwork.com>
2018-12-21 19:25:29 +0530

    till admin middleware

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests;
 use App\User;
 use App\Role;
@@ -40,17 +41,24 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        //       
-        //User::create($request->all());
+
         $user = new User;
-        $input = $request->all();
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+
+        
+       
         if($file = $request->file('photo_id')){
             $name = time() . $file->getClientOriginalName();
             $file->move('images',$name);
             $photo = Photo::create(['file'=>$name]);
             $input['photo_id'] = $photo->id;
         }
-        $input['password'] = bcrypt($request->password);
+        
         $user::create($input);
         return redirect('/admin/users');
     }
@@ -74,7 +82,9 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name','id')->all();
+        return view('admin.users.edit', compact('user','roles'));
     }
 
     /**
@@ -84,9 +94,27 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-        //
+       
+        $user = User::findOrFail($id);
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+        
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images',$name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+       
+        $user->update($input);
+        return redirect('admin/users');
+
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,6 +6,7 @@
     <thead>
       <tr>
         <th>Id</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -20,7 +21,8 @@
 
       <tr>
         <td>{{$user->id}}</td>
-        <td>{{$user->name}}</td>       
+        <td><img src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" width='50px'></td>
+        <td><a href="{{route('admin.users.edit',$user->id)}}">{{$user->name}}</a></td>       
         <td>{{$user->email}}</td>
         <td>{{$user->role_id}}</td>
         <td>{{$user->is_active == 1 ? 'Active' : 'No Active'}}</td>
